c883e41668084076b78c0ec8e821d1a17d6c14c5
Lucas Speers <lucas_speers@outlook.com>
2025-01-13 10:59:33 -0700

    some work

--- src/lex.rs
+++ src/lex.rs
@@ -1,59 +1,24 @@
 use std::{char, iter::Peekable, process::exit, str::Chars};
 
-
-/// The most basic building blocks of a program
 #[derive(Debug, Clone, PartialEq)]
 pub struct Token {
     pub token_type: TokenType,
     /// The (Line, Column) of the token
     pub position: (u64, u64),
     /// The filename where the token originated
-    pub filename: String,
-}
-
-/// hacky way to update the position of the current char
-pub trait CustomNext {
-    fn neext(&mut self, position: &mut (u64, u64)) -> Option<char>;
-}
-impl CustomNext for Peekable<Chars<'_>> {
-    fn neext(&mut self, position: &mut (u64, u64)) -> Option<char> {
-        let value = self.next();
-        if let Some(char) = value {
-            // update the line/column of the token
-            if char == '\n' {
-                position.0 += 1;
-                position.1 = 1;
-            } else {
-                position.1 += 1;
-            }
-        }
-        value
-    }
+    // pub filename: String,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum TokenType {
-    TokNone,
-    TokComment,
-    TokSemicolon,
-    TokSymbol {
-        symbol: Symbol,
-    },
-    TokIdentifier {
-        name: String,
-    },
-    TokNumber {
-        has_decimal: bool,
+    Semicolon,
+    Identifier(String),
+    Number {
         whole: u64,
-        decimal: u64,
+        decimal: Option<u64>,
     },
-    TokString {
-        content: String,
-    }
-}
+    String(String),
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Symbol {
     Addition,
     Subtraction,
     Division,
@@ -61,73 +26,10 @@ pub enum Symbol {
     RightParren,
     LeftCurly,
     RightCurly,
-    Colon,
     Comma,
     Equal,
-    // TODO: {} [] ! ? . < > <= >= etc
-}
-
-impl Symbol {
-    pub fn from(chars: &mut Peekable<Chars>, position: &mut (u64, u64)) -> Option<Symbol> {
-        if let Some(char) = chars.peek() {
-            let symbol: Symbol;
-            match char {
-                '+' => {
-                    chars.neext(position);
-                    return Some(Symbol::Addition);
-                },
-                '-' => {
-                    chars.neext(position);
-                    return Some(Symbol::Subtraction);
-                },
-                '(' => {
-                    chars.neext(position);
-                    return Some(Symbol::LeftParren);
-                },
-                ')' => {
-                    chars.neext(position);
-                    return Some(Symbol::RightParren);
-                },
-                '{' => {
-                    chars.neext(position);
-                    return Some(Symbol::LeftCurly);
-                },
-                '}' => {
-                    chars.neext(position);
-                    return Some(Symbol::RightCurly);
-                },
-                ',' => {
-                    chars.neext(position);
-                    return Some(Symbol::Comma);
-                },
-                ':' => {
-                    chars.neext(position);
-                    return Some(Symbol::Colon);
-                },
-                '=' => {
-                    chars.neext(position);
-                    return Some(Symbol::Equal);
-                },
-                '/' => {
-                    chars.neext(position);
-                    if let Some( next ) = chars.peek() {
-                        if *next == '/' {
-                            loop {
-                                if let Some('\n') = chars.neext(position) {
-                                    return None;
-                                }
-                            }
-                        }
-                    }
-                    return Some(Symbol::Division)
-                }
-                _ => return None
-            }
-        } else {
-            None
-        }
-        
-    }
+    DoubleEqual,
+    // TODO: [] ! ? . < > <= >= etc
 }
 
 fn token_error(position: (u64, u64), filename: &str, error: &str) -> ! {
@@ -140,104 +42,61 @@ fn token_error(position: (u64, u64), filename: &str, error: &str) -> ! {
     exit(1)
 }
 
-pub fn tokenize(input: &str, filename: &str) -> Vec<Token> {
-    use TokenType::*;
-    let mut tokens = Vec::new();
-    let mut iter = input.chars().peekable();
-    let mut position: (u64, u64) = (1,1);
-    let mut token = Token {
-        token_type: TokNone,
-        position,
-        filename: filename.to_owned()
-    };
-    while let Some(&char) = iter.peek() {
-        
-        // the main state machine
-        match &mut token.token_type {
-            TokNone => {
-                if char.is_whitespace() {iter.neext(&mut position);continue;}
-                token.position = position;
-                if char == '"' {
-                    iter.neext(&mut position);
-                    token.token_type = TokString{content: String::new()};
-                } else if char.is_ascii_digit() {
-                    println!("char: {char}");
-                    token.token_type = TokNumber { has_decimal: false, whole: 0, decimal: 0 }
-                } else if char == ';' {
-                    iter.neext(&mut position);
-                    token.token_type = TokSemicolon;
-                } else if let Some(symbol) = Symbol::from(&mut iter, &mut position) {
-                    token.token_type = TokSymbol { symbol };
-                } else {
-                    token.token_type = TokIdentifier { name: String::new() }
-                }
-            },
-            TokComment => {
-                iter.neext(&mut position);
-                if char == '\n' {
-                    token.token_type = TokNone;
-                }
-            }
-            TokSemicolon => {
-                tokens.push(token.clone());
-                token.token_type = TokNone;
-            }
-            TokSymbol { symbol: _ } => {
-                tokens.push(token.clone());
-                token.token_type = TokNone;
-            }
-            TokIdentifier { name } => {
-                if char.is_whitespace() {
-                    iter.neext(&mut position);
-                    tokens.push(token.clone());
-                    token.token_type = TokNone;
-                } else if let Some(symbol) = Symbol::from(&mut iter, &mut position) {
-                    tokens.push(token.clone());
-                    token.token_type = TokSymbol { symbol };
-                } else {
-                    iter.neext(&mut position);
-                    name.push(char);
-                }
-            },
-            TokNumber { has_decimal, whole, decimal } => {
-                if char.is_ascii_digit() {
-                    iter.neext(&mut position);
-                    if !*has_decimal {
-                        *whole *= 10;
-                        *whole += char.to_digit(10).unwrap() as u64;
-                    } else {
-                        *decimal *= 10;
-                        *decimal += char.to_digit(10).unwrap() as u64;
-                    }
-                } else if char == '.' {
-                    iter.neext(&mut position);
-                    *has_decimal = true;
-                } else if char == ';' {
-                    iter.neext(&mut position);
-                    tokens.push(token.clone());
-                    token.token_type = TokSemicolon;
-                } else if let Some(symbol) = Symbol::from(&mut iter, &mut position) {
-                    tokens.push(token.clone());
-                    token.token_type = TokSymbol { symbol };
-                } else {
-                    token.token_type = TokNone;
-                }
-                
-            },
-            TokString { content } => {
-                iter.neext(&mut position);
-                if char == '"' {
-                    tokens.push(token.clone());
-                    token.token_type = TokNone;
-                } else {
-                    content.push(char);
-                }
-            },
-        }
-    }
-    // end of file stuff
-    // match token.token_type
-    
-    tokens
+pub struct Tokenizer {
+    input: Vec<char>,
+    filename: String,
+    pub tokens: Vec<Token>,
+    index: usize,
+    position: (u64, u64),
 }
 
+impl Tokenizer {
+    pub fn new(input: &str, filename: &str) -> Self {
+        Tokenizer { input: input.chars().collect(), filename: filename.into(), tokens: Vec::new(), index: 0, position: (1,1) }
+    }
+    fn get_next(&mut self) -> char {
+        let char = self.input[self.index];
+        self.index += 1;
+        if char == '\n' {self.position.0 += 1; self.position.1 = 0;}
+        else {self.position.1 += 1;}
+        return char;
+    }
+    fn add_token(&mut self, token_type: TokenType) {
+        self.tokens.push(Token { token_type, position: self.position });
+    }
+    pub fn generate(&mut self) {
+        loop {
+            let current_char = self.input[self.index];
+            // ignore whitespace
+            if current_char.is_whitespace() {
+                self.get_next();
+                continue;
+            }
+            // check for comments
+            if current_char == '/' && self.input[self.index+1] == '/' {
+                loop {
+                    if self.get_next() == '\n' {break;} // ignore the rest of the line
+                }
+                continue;
+            }
+            if current_char == '=' && self.input[self.index+1] == '=' {
+                self.add_token(TokenType::DoubleEqual);
+                self.get_next();
+                continue;
+            }
+            match current_char {
+                ';' => self.add_token(TokenType::Semicolon),
+                '+' => self.add_token(TokenType::Addition),
+                '-' => self.add_token(TokenType::Subtraction),
+                '/' => self.add_token(TokenType::Division),
+                '(' => self.add_token(TokenType::LeftParren),
+                ')' => self.add_token(TokenType::RightParren),
+                '{' => self.add_token(TokenType::LeftCurly),
+                '}' => self.add_token(TokenType::RightCurly),
+                ',' => self.add_token(TokenType::Comma),
+                '=' => self.add_token(TokenType::Equal),
+                _ => todo!()
+            }
+        }
+    }
+}
